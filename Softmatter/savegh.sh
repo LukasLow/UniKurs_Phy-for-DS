c59d72f89a4fd707aaab27cd6d6f02262911a9e3
Poda <code@email.lowsky.eu>
2023-07-06 00:08:41 +0200

    Automated commit

--- Softmatter/savegh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /root/vsc_projects/PhyforDS/UniKurs_Phy-for-DS/Softmatter
-git add . || { echo "Failed to add files"; exit 1; }
-
-git commit -m "Automated commit" || { echo "Failed to commit"; exit 2; }
-
-git push origin master || { echo "Failed to push"; exit 3; }
